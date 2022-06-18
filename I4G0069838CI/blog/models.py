5f5b9c6ddf37db9467e48dc0c154dbb8ddddba9a
Amichania <onwubikookem@gmail.com>
2022-06-18 06:30:03 +0100

    This is the full blog app. I think I did it right but mehn, I'm not really sure

---
+++ I4G0069838CI/blog/models.py
@@ -0,0 +1,12 @@
+from unittest.util import _MAX_LENGTH
+from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+# Create your models here.
+class Post(models.Model):
+    Title = models.CharField(max_length = 200)
+    Text = models.TextField()
+    Author = models.ForeignKey(User, on_delete = models.CASCADE,)
+    Created_date = models.DateTimeField()
+    Published_date = models.DateTimeField()
